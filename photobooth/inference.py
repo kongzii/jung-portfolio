12802a81d27e2dff23cd22ff648b0eea6e261688
Peter Jung <peter@jung.ninja>
2023-07-30 16:47:16 +0200

    up

--- photobooth/inference.py
+++ photobooth/inference.py
@@ -1,10 +1,13 @@
 import torch
 from diffusers import DiffusionPipeline
 
-model_dir = "output"
-pipe = DiffusionPipeline.from_pretrained(model_dir)
+model_dir_a = "output"
+pipe = DiffusionPipeline.from_pretrained(model_dir_a, torch_dtype=torch.float16).to(
+    "cuda"
+)
 
-prompt = "A photo of jung in a bucket"
-image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
-
-image.save("test.png")
+while True:
+    prompt = input("Prompt: ")
+    pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0].save(
+        "sample.png"
+    )
